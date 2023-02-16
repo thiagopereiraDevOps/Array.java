d560f4936c3efc647fb49acef132e83e6e78b186
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-02-16 14:01:09 -0300

    adicionar array.java

---
+++ src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        String array[] = {"Hello, World", "Hi there, Everyone", "6"};
+        for (String i : array) {
+            System.out.println(i);
+        }
+    }
+}
